a1b99af10b33c3b9e77dd1fa8eb791915b6c687d
= <mae.vdb@outlook.com>
2023-09-15 21:50:22 +0200

    manually deleted migrations because I'm a wild boar

--- database/migrations/1693733164421_api_tokens.ts
+++ database/migrations/1693733164421_api_tokens.ts
@@ -3,7 +3,7 @@ import BaseSchema from '@ioc:Adonis/Lucid/Schema'
 export default class extends BaseSchema {
   protected tableName = 'api_tokens'
 
-  public async up() {
+	public async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id').primary()
       table.integer('user_id').unsigned().references('id').inTable('users').onDelete('CASCADE')
